e0f6178542a9afa6e60ce73ea9b8408cd2f242ff
Elena Kalugina <rena6@yandex.ru>
2023-10-08 21:15:29 +0300

    Доделала моб меню, добавила для него JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+const menuMain = document.querySelector('.nav__main-list');
+const menuToggle = document.querySelector('.menu-button');
+
+menuToggle.addEventListener('click', function () {
+  if (menuMain.classList.contains('nav__main-list--closed')) {
+    menuMain.classList.remove('nav__main-list--closed');
+    menuMain.classList.add('nav__main-list--opened');
+    menuToggle.classList.remove('menu-button--open');
+    menuToggle.classList.add('menu-button--close');
+  } else {
+    menuMain.classList.remove('nav__main-list--opened');
+    menuMain.classList.add('nav__main-list--closed');
+    menuToggle.classList.remove('menu-button--close');
+    menuToggle.classList.add('menu-button--open');
+  }
+});
